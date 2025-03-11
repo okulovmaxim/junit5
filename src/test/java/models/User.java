9aec211cda93db6922e47f193928938477d29b7c
okulovmaxim <okulov.m.i@gmail.com>
2025-03-11 22:42:46 +0500

    Добавление парамтеров из метода

---
+++ src/test/java/models/User.java
@@ -0,0 +1,19 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    private String email;
+
+    public void defaultUserMethod() {
+        // код
+    }
+}
